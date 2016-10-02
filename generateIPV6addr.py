46bab40a97f900de883e65b86948fc0d64bf6e58
Sudarshan Seshadri <sudarshadri@gmail.com>
2016-10-01 19:55:26 -0700

    Initial commit, code does not by any means perform as intended

---
+++ generateIPV6addr.py
@@ -0,0 +1,13 @@
+output_file = 'IPV6Addresses.txt'
+num_addr = 256
+start_prefix = "20010db885a3000000008a2e0370"
+curr_suffix = 0
+addresses = []
+
+### simply increments the address by 1
+for _ in range(num_addr):
+	addresses.append(start_prefix + "%0.4x" % curr_suffix + '\n')
+	curr_suffix += 1
+
+with open(output_file, 'w') as f:
+	f.write(''.join(addresses))
